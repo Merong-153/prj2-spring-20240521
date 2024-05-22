850aa1322ba19714404dc21f13b5e5c1b3e7a037
mj <wmj0924@gmail.com>
2024-05-22 12:36:56 +0900

    20240522

---
+++ src/main/java/com/prj2spring20240521/controller/member/MemberController.java
@@ -0,0 +1,43 @@
+package com.prj2spring20240521.controller.member;
+
+import com.prj2spring20240521.domain.member.Member;
+import com.prj2spring20240521.service.member.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/member")
+public class MemberController {
+
+    final MemberService service;
+
+    @PostMapping("signup")
+    public ResponseEntity signup(@RequestBody Member member) {
+        if (service.validate(member)) {
+            service.add(member);
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @GetMapping(value = "check", params = "email")
+    public ResponseEntity checkEmail(@RequestParam("email") String email) {
+        Member member = service.getByEmail(email);
+        if (member == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(email);
+    }
+
+    @GetMapping(value = "check", params = "nickName")
+    public ResponseEntity checkNickName(@RequestParam("nickName") String nickName) {
+        Member member = service.getByNickName(nickName);
+        if (member == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(nickName);
+    }
+}
